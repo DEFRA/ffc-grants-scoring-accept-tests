0ea8a83e9c2bee6e55787e4a4fba4b608544ae3d
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-01-22 09:29:26 +0000

    Add score endpoint tests

--- .jest/setup.js
+++ .jest/setup.js
@@ -1,3 +1,1 @@
-global.baseUrl = process.env.ENVIRONMENT
-  ? `https://ffc-grants-scoring.${process.env.ENVIRONMENT}.cdp-int.defra.cloud`
-  : 'http://localhost:3555'
+global.baseUrl = `https://ffc-grants-scoring.dev.cdp-int.defra.cloud`
