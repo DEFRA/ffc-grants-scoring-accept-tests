60be1c27bfb74af006340af4abec13db0ab24df1
Andrew Deacon <andrew.deacon@equalexperts.com>
2025-05-15 06:02:23 +0100

    Refactor and create failing test for summed score

--- test/specs/adding-value/scoring.spec.js
+++ test/specs/adding-value/scoring.spec.js
@@ -1,65 +1,6 @@
 import request from 'supertest'
 
 describe('Scoring', () => {
-  it('should score a singleScore question', async () => {
-    const payload = {
-      data: {
-        main: {
-          futureCustomersRadiosField: 'futureCustomers-A3'
-        }
-      }
-    }
-
-    const res = await request(global.baseUrl)
-      .post('/scoring/api/v1/adding-value/score?allowPartialScoring=true')
-      .send(payload).set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-
-    expect(res.status).toEqual(200)
-    expect(res.body.answers.find((a) => a.questionId === 'futureCustomersRadiosField').score.value).toBe(7)
-    expect(res.body.answers.find((a) => a.questionId === 'futureCustomersRadiosField').score.band).toBe('Medium')
-  })
-
-  it('should score a multiScore question', async () => {
-    const payload = {
-      data: {
-        main: {
-          projectImpactCheckboxesField: ['projectImpact-A1', 'projectImpact-A2', 'projectImpact-A4']
-        }
-      }
-    }
-
-    const res = await request(global.baseUrl)
-      .post('/scoring/api/v1/adding-value/score?allowPartialScoring=true')
-      .send(payload)
-      .set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-
-    expect(res.status).toEqual(200)
-    expect(res.body.answers.find((a) => a.questionId === 'projectImpactCheckboxesField').score.value).toBe(17)
-    expect(res.body.answers.find((a) => a.questionId === 'projectImpactCheckboxesField').score.band).toBe('Strong')
-  })
-
-  it('should score a matrixScore question', async () => {
-    const payload = {
-      data: {
-        main: {
-          produceProcessedRadiosField: 'produceProcessed-A3',
-          howAddingValueRadiosField: 'howAddingValue-A2'
-        }
-      }
-    }
-
-    const res = await request(global.baseUrl)
-      .post('/scoring/api/v1/adding-value/score?allowPartialScoring=true')
-      .send(payload).set('Content-Type', 'application/json')
-      .set('Accept', 'application/json')
-
-    expect(res.status).toEqual(200)
-    expect(res.body.answers.find((a) => a.questionId === 'produceProcessedRadiosField').score.value).toBe(18)
-    expect(res.body.answers.find((a) => a.questionId === 'produceProcessedRadiosField').score.band).toBe('Medium')
-  });
-
   it('should score a full set of questions and answers', async () => {
     const payload = {
       data: {
@@ -81,9 +22,9 @@ describe('Scoring', () => {
       .set('Accept', 'application/json')
 
     expect(res.status).toEqual(200)
-    //expect(res.score).toEqual(66) // or maybe 48?
-    //expect(res.status).toEqual('Ineligible') // 48 is ineligible, but 60 is eligible
-    //expect(res.scoreBand).toEqual('Medium') // is this correct?
+    expect(res.body.score).toEqual(48)
+    expect(res.body.status).toEqual('Ineligible')
+    expect(res.body.scoreBand).toEqual('Weak')
     expect(res.body.answers.find((a) => a.questionId === 'produceProcessedRadiosField').score.value).toBe(18)
     expect(res.body.answers.find((a) => a.questionId === 'produceProcessedRadiosField').score.band).toBe('Medium')
     expect(res.body.answers.find((a) => a.questionId === 'projectImpactCheckboxesField').score.value).toBe(11)
@@ -122,7 +63,21 @@ describe('Scoring', () => {
     // validate response against schema?
   })
 
-  it('should not score a single question if allowPartialScoring parameter is not set', async () => {
+  it('should return 400 when input does not conform to expected JSON format', async () => {
+    const payload = {
+      invalid: 'invalid'
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/adding-value/score')
+      .send(payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(res.status).toEqual(400)
+  })
+
+  it('should return 400 when all scoring questions are not supplied', async () => {
     const payload = {
       data: {
         main: {
@@ -138,5 +93,129 @@ describe('Scoring', () => {
       .set('Accept', 'application/json')
 
     expect(res.status).toEqual(400)
+  })
+
+  it('should return 400 when answers do not match the scoring config', async () => {
+    const payload = {
+      data: {
+        main: {
+          produceProcessedRadiosField: 'produceProcessed-A50',
+          howAddingValueRadiosField: 'howAddingValue-A2',
+          projectImpactCheckboxesField: ['projectImpact-A2', 'projectImpact-A3'],
+          futureCustomersRadiosField: 'futureCustomers-A1',
+          collaborationRadiosField: 'collaboration-A2',
+          environmentalImpactCheckboxesField: ['environmentalImpact-A6', 'environmentalImpact-A7']
+        }
+      }
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/adding-value/score')
+      .send(payload).set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(res.status).toEqual(400)
+  })
+
+  it('should return 400 when invalid grant type given in URL', async () => {
+    const payload = {
+      data: {
+        main: {
+          produceProcessedRadiosField: 'produceProcessed-A3',
+          howAddingValueRadiosField: 'howAddingValue-A2',
+          projectImpactCheckboxesField: ['projectImpact-A1', 'projectImpact-A2'],
+          futureCustomersRadiosField: 'futureCustomers-A1',
+          collaborationRadiosField: 'collaboration-A2',
+          environmentalImpactCheckboxesField: ['environmentalImpact-A6', 'environmentalImpact-A7']
+        }
+      }
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/invalid-grant/score')
+      .send(payload).set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(res.status).toEqual(400)
+  })
+
+  it('should return 400 when invalid grant type given in URL alongside duplicate answers', async () => {
+    const payload = {
+      data: {
+        main: {
+          produceProcessedRadiosField: 'produceProcessed-A3',
+          howAddingValueRadiosField: 'howAddingValue-A2',
+          projectImpactCheckboxesField: ['projectImpact-A1', 'projectImpact-A1'],
+          futureCustomersRadiosField: 'futureCustomers-A1',
+          collaborationRadiosField: 'collaboration-A2',
+          environmentalImpactCheckboxesField: ['environmentalImpact-A6', 'environmentalImpact-A7']
+        }
+      }
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/invalid-grant/score')
+      .send(payload).set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(res.status).toEqual(400)
+  })
+
+  it('should return 400 when invalid query string parameter is given', async () => {
+    const payload = {
+      data: {
+        main: {
+          produceProcessedRadiosField: 'produceProcessed-A3',
+          howAddingValueRadiosField: 'howAddingValue-A2',
+          projectImpactCheckboxesField: ['projectImpact-A2', 'projectImpact-A3'],
+          futureCustomersRadiosField: 'futureCustomers-A1',
+          collaborationRadiosField: 'collaboration-A2',
+          environmentalImpactCheckboxesField: ['environmentalImpact-A6', 'environmentalImpact-A7']
+        }
+      }
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/adding-value/score?invalid=true')
+      .send(payload).set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(res.status).toEqual(400)
+  })
+
+  it('should return 400 when lone question is sent and allowPartialScoring parameter is not set', async () => {
+    const payload = {
+      data: {
+        main: {
+          futureCustomersRadiosField: 'futureCustomers-A3'
+        }
+      }
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/adding-value/score')
+      .send(payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(res.status).toEqual(400)
+  })
+
+  it('should return 400 when lone question is sent and allowPartialScoring parameter is false', async () => {
+    const payload = {
+      data: {
+        main: {
+          futureCustomersRadiosField: 'futureCustomers-A3'
+        }
+      }
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/adding-value/score?allowPartialScoring=false')
+      .send(payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+      expect(res.status).toEqual(400)
   })
 })
---
+++ test/specs/adding-value/single-scoring.spec.js
@@ -0,0 +1,68 @@
+import request from 'supertest'
+
+describe('Single Scoring', () => {
+  it('should score a singleScore question', async () => {
+    const payload = {
+      data: {
+        main: {
+          futureCustomersRadiosField: 'futureCustomers-A3'
+        }
+      }
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/adding-value/score?allowPartialScoring=true')
+      .send(payload).set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(res.status).toEqual(200)
+    expect(res.body.answers.find((a) => a.questionId === 'futureCustomersRadiosField').score.value).toBe(7)
+    expect(res.body.answers.find((a) => a.questionId === 'futureCustomersRadiosField').score.band).toBe('Medium')
+  })
+
+  it('should return 400 when multiple answers are given to a singleScore question', async () => {
+    const payload = {
+      data: {
+        main: {
+          produceProcessedRadiosField: 'produceProcessed-A3',
+          howAddingValueRadiosField: 'howAddingValue-A2',
+          projectImpactCheckboxesField: ['projectImpact-A2', 'projectImpact-A3'],
+          futureCustomersRadiosField: ['futureCustomers-A1', 'futureCustomers-A2'],
+          collaborationRadiosField: 'collaboration-A2',
+          environmentalImpactCheckboxesField: ['environmentalImpact-A6', 'environmentalImpact-A7']
+        }
+      }
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/adding-value/score')
+      .send(payload)
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(res.status).toEqual(400)
+  })
+
+  it('should return 400 when no answer is given to a singleScore question', async () => {
+    const payload = {
+      data: {
+        main: {
+          produceProcessedRadiosField: 'produceProcessed-A3',
+          howAddingValueRadiosField: 'howAddingValue-A2',
+          projectImpactCheckboxesField: ['projectImpact-A2', 'projectImpact-A3'],
+          futureCustomersRadiosField: null,
+          collaborationRadiosField: 'collaboration-A2',
+          environmentalImpactCheckboxesField: ['environmentalImpact-A6', 'environmentalImpact-A7']
+        }
+      }
+    }
+
+    const res = await request(global.baseUrl)
+      .post('/scoring/api/v1/adding-value/score')
+      .send(payload).set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+
+    expect(res.status).toEqual(400)
+  })
+  
+})
